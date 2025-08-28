6e32a5482a16e653cb10a84150e3cbf151a40c4f
Maximiliano Davini <maximiliano.davini@gmail.com>
2025-08-28 15:43:21 -0300

    Up

---
+++ Backup/PracticaProfesional2025/ConnectionFactory.cs
@@ -0,0 +1,11 @@
+﻿using System.Configuration;
+using System.Data.SqlClient;
+
+public static class ConnectionFactory
+{
+    public static SqlConnection GetConnection()
+    {
+        string connectionString = ConfigurationManager.ConnectionStrings["ezeDDBB"].ConnectionString;
+        return new SqlConnection(connectionString);
+    }
+}
