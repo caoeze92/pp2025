7ef5f63e0c44e43a746d2ad9acdb1dab276b5c1a
ezecao <ezecao@bancocredicoop.coop>
2025-08-31 12:38:25 -0300

    fix conf bbdd e ignore a binario

--- PracticaProfesional2025/ConnectionFactory.cs
+++ PracticaProfesional2025/ConnectionFactory.cs
@@ -5,7 +5,7 @@ public static class ConnectionFactory
 {
     public static SqlConnection GetConnection()
     {
-        string connectionString = ConfigurationManager.ConnectionStrings["ezeDDBB"].ConnectionString;
+        string connectionString = ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString;
         return new SqlConnection(connectionString);
     }
 }
