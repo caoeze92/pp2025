3b9c27320abac25a6c45495c523bc0e11b05e8b7
unknown <nicolasautalan@gmail.com>
2025-09-01 17:29:04 -0300

    Agrego Administracion de Usuarios

---
+++ PracticaProfesional2025/AdministracionDeUsuarios.aspx
@@ -0,0 +1,43 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="AdministracionDeUsuarios.aspx.cs" Inherits="PracticaProfesional2025.ModificarUsuario" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head id="Head1" runat="server">
+    <title>Administración De Usuarios</title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:GridView ID="gvUsuarios" runat="server" AutoGenerateColumns="false" DataKeyNames="id_usuario" OnRowEditing="gvUsuarios_RowEditing" OnRowCancelingEdit="gvUsuarios_RowCancelingEdit" OnRowUpdating="gvUsuarios_RowUpdating" OnRowDeleting="gvUsuarios_RowDeleting">
+            <Columns>
+                <asp:BoundField DataField="id_usuario" HeaderText="ID" ReadOnly="true" />
+                <asp:BoundField DataField="nombre" HeaderText="Nombre" />
+                <asp:BoundField DataField="apellido" HeaderText="Apellido" />
+                <asp:BoundField DataField="email" HeaderText="Email" />
+                <asp:BoundField DataField="telefono" HeaderText="Telefono" />
+                <asp:BoundField DataField="activo" HeaderText="Activo" />
+
+                <asp:TemplateField HeaderText="Rol">
+                    <EditItemTemplate>
+                        <asp:DropDownList ID="ddlRol" runat="server">
+                            <asp:ListItem Text="admin" Value="admin" />
+                            <asp:ListItem Text="user" Value="user" />
+                            <asp:ListItem Text="invitado" Value="invitado" />
+                        </asp:DropDownList>
+                    </EditItemTemplate>
+                    <ItemTemplate>
+                        <%# Eval("Rol") %>
+                    </ItemTemplate>
+                </asp:TemplateField>
+
+             <asp:CommandField ShowEditButton="true" ShowDeleteButton="true" /> 
+
+            </Columns>
+        
+        
+        </asp:GridView>
+    </div>
+    </form>
+</body>
+</html>
---
+++ PracticaProfesional2025/AdministracionDeUsuarios.aspx.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace PracticaProfesional2025
+{
+    public partial class ModificarUsuario : System.Web.UI.Page
+    {
+        string connStr = ConfigurationManager.ConnectionStrings["Conexion"].ConnectionString;
+
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Session["Rol"] == null || Session["Rol"].ToString() != "admin")
+            {
+                Response.Redirect("NoAutorizado.aspx");
+
+            }
+
+            if (IsPostBack)
+            {
+                CargarUsuarios();
+            }
+        }
+
+        private void CargarUsuarios()
+        {
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                SqlDataAdapter da = new SqlDataAdapter(" SELECT id_usuario, nombre, apellido, rol, email, telefono, activo FROM Usuario", conn);
+                DataTable dt = new DataTable();
+                da.Fill(dt);
+                gvUsuarios.DataSource = dt;
+                gvUsuarios.DataBind();
+            }
+        }
+
+        protected void gvUsuarios_RowEditing(object sender, System.Web.UI.WebControls.GridViewEditEventArgs e)
+        {
+            gvUsuarios.EditIndex = e.NewEditIndex;
+            CargarUsuarios();
+        }
+
+        protected void gvUsuarios_RowCancelingEdit(object sender, System.Web.UI.WebControls.GridViewCancelEditEventArgs e)
+        {
+            gvUsuarios.EditIndex = -1;
+            CargarUsuarios();
+        }
+
+        protected void gvUsuarios_RowUpdating(object sender, System.Web.UI.WebControls.GridViewUpdateEventArgs e)
+        {
+            int idUsuario = (int)gvUsuarios.DataKeys[e.RowIndex].Value;
+            GridViewRow row = gvUsuarios.Rows[e.RowIndex];
+            DropDownList ddlRol = (DropDownList)row.FindControl("ddlRol");
+
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("UPDATE Usuarios SET rol=@rol WHERE id_usuario=@ID", conn);
+                cmd.Parameters.AddWithValue("@Rol", ddlRol.SelectedValue);
+                cmd.Parameters.AddWithValue("@ID", idUsuario);
+                cmd.ExecuteNonQuery();
+            }
+            gvUsuarios.EditIndex = -1;
+            CargarUsuarios();
+        }
+
+        protected void gvUsuarios_RowDeleting(object sender, System.Web.UI.WebControls.GridViewDeleteEventArgs e)
+        {
+            int idUsuario = (int)gvUsuarios.DataKeys[e.RowIndex].Value;
+
+            using (SqlConnection conn = new SqlConnection(connStr))
+            {
+                conn.Open();
+                SqlCommand cmd = new SqlCommand("DELETE FROM Usuarios WHERE id_usuario=@ID", conn);
+                cmd.Parameters.AddWithValue("@ID", idUsuario);
+                cmd.ExecuteNonQuery();
+            }
+
+            CargarUsuarios();
+        }
+    }
+}
